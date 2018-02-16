20acf148bee505e7f8103f1da60a4b9daf19b1ef
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-02-16 14:17:24 -0500

    Added Nonvolatile Memory subsystem

---
+++ src/nonvolatile_memory.c
@@ -0,0 +1,246 @@
+#include "includes.h"
+
+/* ************************************************************************** */
+
+/*  nvm_write() is the 'engage' button for writing to both types of nonvolatile
+    memory.  
+    
+    This function is the smallest unit that needs to be marked critical.
+*/
+void nvm_write(void)
+{
+    NVMCON1bits.WREN = 1; // Enable NVM writes
+    
+    // Magic Sequence - Do Not Change
+    #asm
+        MOVLW 0x55
+        MOVWF NVMCON2
+        MOVLW 0xAA
+        MOVWF NVMCON2
+        BSF   NVMCON1, 1 ;// sets WR bit, starting the write
+    #endasm
+    
+    NVMCON1bits.WREN = 0; // Disable NVM writes
+}
+
+/*  Notes on PIC18's EEPROM Memory:
+    
+    EEPROM in PIC18 family MCUs is very straightforward.
+    There is 256 or 1024 bytes of byte-addressable EEPROM.
+    
+    Both read and write operations follow the same basic sequence, with the
+    major difference being time required.  Reading from EEPROM only takes one
+    cycle after the operation is started.  Writing to EEPROM takes up to 4ms.
+    Other user code could be executed during that time, but in this application
+    we will choose to wait for it to finish.
+    
+    Read:
+    1) Load the desired address into EEADR(and EEADRH)
+    2) Set the RD bit of EECON1, to begin operation
+    3) Read byte from EEDATA
+    
+    Write:
+    1) Load the desired address into EEADR(and EEADRH)
+    2) Load the byte to be written into EEDATA
+    3) Set the WREN bit of EECON1, to enable writes
+    4) Write the 'magic sequence' of 0x55, 0xAA to EECON2, to unlock writes
+    5) Set the WR bit of EECON1, to begin operation
+    6) Wait for WR bit to be cleared by hardware
+*/
+
+uint8_t internal_eeprom_read(uint16_t address)
+{
+    #if LOG_LEVEL_EEPROM >= LOG_INFO
+    print_str_ln("eeprom_read");
+    #endif
+    
+    // Load lower byte of address into register
+    NVMADRL = address;
+    // If EEADRH is present, load high byte of address into register
+    #if EEADRH
+    NVMADRH = address >> 8;
+    #endif
+    
+    // Select EEPROM
+    NVMCON1bits.REG = 0;
+    // Initiate read operation
+    NVMCON1bits.RD = 1;
+    
+    // Return the value
+    return (NVMDAT);
+}
+
+void internal_eeprom_write(uint16_t address, uint8_t value)
+{
+    #if LOG_LEVEL_EEPROM >= LOG_INFO
+    print_str_ln("eeprom_write");
+    #endif
+    // Wait for possible previous write to complete
+    while(NVMCON1bits.WR);
+    
+    // Load lower byte of address into register
+    NVMADRL = address;
+    // If EEADRH is present, load high byte of address into register
+    #if EEADRH
+    NVMADRH = address >> 8;
+    #endif
+    
+    // Load value into register
+    NVMDAT = value;
+    
+    // Select EEPROM
+    NVMCON1bits.REG = 0;
+    
+    // Engage
+    nvm_write();
+}
+
+/* ************************************************************************** */
+
+/*  Notes on PIC18's Flash memory:
+    
+    Flash memory can be read as individual bytes by loading a desired address
+    into TBLPTR and executing a TBLRD* asm instruction.  The result of the read
+    operation is placed into the TABLAT register.
+    
+    Flash memory can only be written in 64 byte blocks.  In order to write only
+    one or two bytes to flash without corrupting surrounding values, the 
+    following steps are necessary:
+        1) Declare a buffer to hold 64 bytes.
+        2) Call flash_block_read() with the desired address and buffer.
+        3) Modify the buffer's contents with the new values.
+        4) Call flash_block_write() to write the new buffer into the block.
+        5) Verify that the block was written to correctly.
+    
+    Registers involved in Flash memory operations:
+    
+    TBLPTR:
+    
+    TABLAT: 
+    
+    EECON1: control bits for Flash operations
+        EEPGD: Select Flash or EEPROM 
+        CFGS: Select Flash/EEPROM or Config Bits
+        n/a
+        FREE: 
+        WRERR: 
+        WREN: 
+        WR: Write Control, set to initiate write
+        RD: Read Control, set to initiate read
+    EECON2: recieves the 'magic sequence' to allow flash writes
+
+*/
+
+uint8_t flash_read(uint32_t address)
+{
+    #if LOG_LEVEL_FLASH >= LOG_INFO
+    print_str_ln("flash_read");
+    #endif
+    
+    // Load the address into the tablepointer registers
+    TBLPTRL = address;
+    TBLPTRH = address >> 8;
+    TBLPTRU = address >> 16;
+
+    // Read one byte at the given address
+    asm("TBLRD*+");
+
+    return TABLAT;
+}
+
+void flash_block_read(uint32_t address, uint8_t *buffer)
+{
+    uint8_t i = 64;
+    uint32_t blockAddress;
+    
+    #if LOG_LEVEL_FLASH >= LOG_INFO
+    print_str_ln("flash_block_read");
+    #endif
+    
+    // Mask off the block address
+    blockAddress = address & 0xffffffc0;
+    
+    // Load the address into the tablepointer registers
+    TBLPTRL = blockAddress;
+    TBLPTRH = blockAddress >> 8;
+    TBLPTRU = blockAddress >> 16;
+
+    while(i--)
+    {
+        asm("TBLRD*+");
+        *buffer++ = TABLAT;
+    }
+}
+
+void flash_block_erase(uint32_t address)
+{
+    #if LOG_LEVEL_FLASH >= LOG_INFO
+    print_str_ln("flash_block_erase");
+    #endif
+    
+    // Load the address into the tablepointer registers
+    TBLPTRL = address;
+    TBLPTRH = address >> 8;
+    TBLPTRU = address >> 16;
+
+    // Helmsman, engage
+    NVMCON1bits.REG = 1;
+    NVMCON1bits.FREE = 1;
+    nvm_write();
+}
+
+void flash_block_write(uint32_t address, uint8_t *buffer)
+{
+    uint8_t i = 0;
+    uint32_t blockAddress;
+    
+    #if LOG_LEVEL_FLASH >= LOG_INFO
+    print_str_ln("flash_block_write");
+    #endif
+
+    // Mask off the block address
+    blockAddress = address & 0xffffffc0;
+    
+    #if LOG_LEVEL_FLASH >= LOG_DETAILS
+    print_cat_ln("  address = ", address);
+    print_cat_ln("  blockAddress = ", blockAddress);
+    #endif
+    
+    // Load the address into the tablepointer registers
+    TBLPTRL = blockAddress;
+    TBLPTRH = blockAddress >> 8;
+    TBLPTRU = blockAddress >> 16;
+
+    // Load the first half-block into the write buffer
+    while(i < 32)
+    {
+        TABLAT = buffer[i++];
+        asm("TBLWT*+");
+    }
+    
+    // Decrement the tablepointer before nvm_write()
+    asm("TBLRD*-");
+    
+    // Helmsman, engage
+    NVMCON1bits.REG = 0b10;
+    NVMCON1bits.FREE = 0;
+    nvm_write();
+
+    // Incrememnt the tablepointer to put it back where it was
+    asm("TBLRD*+");
+    
+    // Load the second half-block into the write buffer
+    while(i < 64)
+    {
+        TABLAT = buffer[i++];
+        asm("TBLWT*+");
+    }
+    
+    // Decrement the tablepointer before nvm_write()
+    asm("TBLRD*-");
+    
+    // Helmsman, engage
+    NVMCON1bits.REG = 0b10;
+    NVMCON1bits.FREE = 0;
+    nvm_write();
+}
---
+++ src/nonvolatile_memory.h
@@ -0,0 +1,87 @@
+#ifndef _NONVOLATILE_MEMORY_H_
+#define _NONVOLATILE_MEMORY_H_
+
+/* ************************************************************************** */
+
+/*  Notes on using EEPROM Memory operations:
+    
+    EEPROM can be read AND written as individual bytes, no block operations are
+    needed.
+    
+    eeprom_read() is very fast, the read operation itself takes a single cycle.
+    
+    eeprom_write() can be quite a bit slower.  The write operation can take as 
+    long as 4ms to complete.  eeprom_write() does NOT wait for the write
+    operation to complete, instead returning as soon as the write is engaged.
+    In order to prevent conflict with multiple back-to-back writes, the relevant
+    flag is checked at the beginning of the function.  If a previous write is
+    still in process, it will wait until it completes before continuing with the
+    new write operation.
+    
+    In order to preserve the limited number of EEPROM write/erase cycles,
+    eeprom_write() first reads the provided address and compares the stored
+    value with the value that is to be stored.  It only writes if the two do not
+    match.
+    
+    PIC18F2X/4XK22 devices have either 256 or 1024 bytes of EEPROM.  The address
+    is an unsigned 16 bit integer to accomodate the larger devices.  If a model
+    with 256 bytes of EEPROM is being used, feel free to change this code to
+    use a uint8_t.
+*/
+
+// Read one byte from EEPROM memory at (address)
+extern uint8_t internal_eeprom_read(uint16_t address);
+
+// Write one byte to EEPROM memory at (address)
+extern void internal_eeprom_write(uint16_t address, uint8_t c);
+
+/* ************************************************************************** */
+
+/*  Notes on using Flash Memory operations on the PIC18F4520
+    
+    Flash memory can be read as individual bytes, must be erased in 64 byte
+    blocks, and can only be written in 32 byte blocks.
+    
+    The flash_block_xxxx() functions are designed to facilitate this block
+    behavior.  They automatically align all operations to block boundaries, so
+    any address in a given block will produce the same results.
+    
+    Recommended procedure for writing to flash memory is as follows:
+    
+    void example_write(uint32_t address, uint8_t value1, uint8_t value2)
+    {
+        uint8_t buffer[64];
+        uint8_t *pointer;
+        uint8_t i;
+    
+        pointer = buffer;
+        
+        // Read existing block into buffer
+        flash_block_read(address, pointer);
+        
+        // Mask off everything but bottom 6 bits(64 addresses)
+        i = address & 0x003f;
+        
+        // Write the two new values into the buffer at the correct addresses
+        buffer[i] = value1;
+        buffer[i + 1] = value2;    
+        
+        // Write the edited buffer into flash    
+        flash_block_write(address, pointer);
+    }
+*/
+
+// Read one byte from Flash memory at (address)
+uint8_t flash_read(uint32_t address);
+
+// Read an entire block of 64 bytes from Flash memory into the provided buffer
+void flash_block_read(uint32_t address, uint8_t *buffer);
+
+// Erase a block of Flash memory at (address)
+void flash_block_erase(uint32_t address);
+
+// Write the provided buffer into flash memory at (address)
+void flash_block_write(uint32_t address, uint8_t *buffer);
+
+
+#endif
