84ecfd697863296bfbbc646fed1a6c2e262c7c20
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-02-16 14:37:12 -0500

    Copied SWR code from YT-1200 and SWR Threshold code from Z-11ProII

--- src/RF_sensor.c
+++ src/RF_sensor.c
@@ -5,6 +5,12 @@
 // Global RF Readings
 RF_power_s currentRF;
 
+// Global SWR Threshold Settings
+uint16_t        swrThresh = SWR1_7;
+uint8_t         swrThreshIndex = 0;
+const uint16_t  swrThreshTable[5] = {SWR1_5, SWR1_7, SWR2_0, SWR2_5, SWR3_0};
+
+
 /* ************************************************************************** */
 
 void RF_sensor_init(void)
@@ -14,7 +20,22 @@ void RF_sensor_init(void)
     currentRF.forward = 0;
     currentRF.reverse = 0;
     currentRF.swr = 0;
-    currentRF.period = 0xffff;
+    currentRF.frequency = 0xffff;
+}
+
+void SWR_threshold_set(void)
+{
+    swrThresh = swrThreshTable[swrThreshIndex];
+}
+
+void SWR_threshold_increment(void)
+{
+    swrThreshIndex++;
+    if (swrThreshIndex == 5)
+    {
+        swrThreshIndex = 0;
+    }
+    SWR_threshold_set();
 }
 
 /* -------------------------------------------------------------------------- */
@@ -38,4 +59,109 @@ uint16_t get_freq(void)
     
 
     return timer1_read();
+}
+
+/* -------------------------------------------------------------------------- */
+
+/*  SWR_average() calculates the average SWR across several samples
+    
+    Several alternative techniques were considered, including collecting an
+    array of samples, possibly discarding outliers, then calculating an adjusted
+    or otherwise filtered mean.
+    
+    Possible future improvements include fixed-point math or one of several
+    techniques including Exponential Moving Average or an Infinite or Finite
+    Impulse Response (IIR) filter
+*/
+
+#define NUM_OF_SAMPLES 8
+#define SAMPLE_FACTOR 3
+
+void SWR_average(void)
+{
+    uint8_t i = 0;
+    uint16_t tempFWD = 0;
+    uint16_t tempREV = 0;
+    uint16_t tempSWR = 0;
+
+    #if LOG_LEVEL_RF_SENSOR >= LOG_LABELS
+    print_format(BRIGHT, BLUE);
+    print_str_ln("\tSWR_average");
+    #endif
+    
+    for (i = 0; i < NUM_OF_SAMPLES; i++)
+    {
+        tempFWD += adc_measure(0);
+        tempREV += adc_measure(1);
+    }
+    tempFWD >>= SAMPLE_FACTOR;
+    tempREV >>= SAMPLE_FACTOR; 
+    
+    if (tempFWD > 8) {
+        tempFWD >>= 2;
+        tempREV >>= 2; 
+        
+        currentRF.forward = tempFWD;
+        currentRF.reverse = tempREV; 
+        
+        tempREV <<= 8; 
+        tempSWR = tempREV / tempFWD;
+        currentRF.swr = tempSWR;
+    } else {
+        currentRF.forward = 0;
+        currentRF.reverse = 0;
+        currentRF.swr = 0xffff;
+    }
+    return;
+}
+
+// abs() is borrowed from the sdcc stdlib
+int16_t abs(int16_t j)
+{
+    return (j < 0) ? -j : j;
+}
+
+#define NO_POWER_COUNT 5000
+#define LOW_POWER 30
+
+int8_t SWR_stable_average(void)
+{
+    uint16_t ADcount = 0;
+    int16_t currentFWD;
+    int16_t previousFWD;
+    int16_t deltaFWD = 0;
+    int16_t deltaCompare = 0;
+
+    #if LOG_LEVEL_RF_SENSOR >= LOG_LABELS
+    print_format(BRIGHT, BLUE);
+    print_str_ln("\tSWR_stable_average");
+    #endif
+    
+    previousFWD = adc_measure(0);
+    while(1)
+    {
+        currentFWD = adc_measure(0);
+        deltaFWD = abs(currentFWD - previousFWD);
+        deltaCompare = currentFWD >> 4;
+        
+        if (currentFWD > LOW_POWER) {
+            if (deltaFWD < deltaCompare) break;
+        }
+        if (ADcount > NO_POWER_COUNT) return (-1);
+        
+        previousFWD = currentFWD;
+        ADcount++;
+    }
+    
+    #if LOG_LEVEL_RF_SENSOR >= LOG_ERROR
+    if (ADcount != 0) 
+    {
+        print_format(BRIGHT, CYAN);
+        print_cat_ln("\t\tADcount: ", ADcount);
+    }
+    #endif
+    
+    SWR_average();
+    
+    return 0;
 }
--- src/RF_sensor.h
+++ src/RF_sensor.h
@@ -3,26 +3,71 @@
 
 /* ************************************************************************** */
 
+// Swr computation result definitions. Definitely hardware dependent!
+#define SWR1_0 0
+#define SWR1_1 1
+#define SWR1_3 7
+#define SWR1_5 10
+#define SWR1_7 20
+#define SWR2_0 40
+#define SWR2_5 75
+#define SWR3_0 100
+#define SWR3_5 120
+
+/* ************************************************************************** */
+
+// This is a little above the 125W values below, and will hopefully not trigger
+// a false positive
+#define HIGH_OVER_POWER     235
+
+// 125W on Alpha Power 4510A, from Kenwood TS-480X into 50ohm dummy load
+// Updated 3/20/17 to adjust for new SWR math
+#define OVER_POWER_2MHZ     171
+#define OVER_POWER_3MHZ     202
+#define OVER_POWER_5MHZ     212
+#define OVER_POWER_7MHZ     215
+#define OVER_POWER_10MHZ    218
+#define OVER_POWER_14MHZ    220
+#define OVER_POWER_18MHZ    216
+#define OVER_POWER_21MHZ    216
+#define OVER_POWER_24MHZ    218
+#define OVER_POWER_28MHZ    215
+
+/* ************************************************************************** */
+
 typedef union {
     struct {
         uint16_t forward;
         uint16_t reverse;
         uint16_t swr;
-        uint16_t period;
+        uint16_t frequency;
     };
 } RF_power_s;
 
 // Global RF Readings
 extern RF_power_s currentRF;
 
+// Global SWR Threshold Settings
+extern uint16_t swrThresh;
+extern uint8_t swrThreshIndex;
+
 /* ************************************************************************** */
 
 extern void RF_sensor_init(void);
+
+// SWR Threshold manipulation
+extern void SWR_threshold_set(void);
+extern void SWR_threshold_increment(void);
 
 /* -------------------------------------------------------------------------- */
 
 // Frequency measurement
 extern uint16_t get_freq(void);
 extern uint16_t get_period(void);
+
+// SWR measurement functions
+extern void SWR_average(void);
+extern int8_t SWR_stable_average(void);
+
 
 #endif
