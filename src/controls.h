c9c08d6152315a36e081452c2069333350273e4b
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-02-16 23:33:21 -0500

    Removed functions that no longer need to be externally visible

--- src/controls.h
+++ src/controls.h
@@ -11,23 +11,6 @@
 
 /* ************************************************************************** */
 
-//
-extern void short_tune_release(void);
-extern void medium_tune_release(void);
-extern void long_tune_release(void);
-
-// Front panel control handlers
-extern void toggle_bypass(void);
-extern void toggle_auto(void);
-extern void toggle_hiloz(void);
-extern void manual_store(void);
-
-// 
-extern void mode_thresh(void);
-extern void mode_func(void);
-
-/* -------------------------------------------------------------------------- */
-
 extern void cup_hold(void);
 extern void cdn_hold(void);
 extern void lup_hold(void);
