4bd3c4d6024255ac25bf1aedc81afc13f2de9af6
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-03-30 23:11:17 -0400

    Test code removed from main()

--- src/main.c
+++ src/main.c
@@ -6,23 +6,6 @@ void main(void)
 {
     startup();
 
-
-
-    // while(1)
-    // {
-    //     spi_tx_word(0xffff);
-    //     delay_ms(100);
-    //     spi_tx_word(0x0000);
-    //     delay_ms(100);
-    // }
-    // while(1)
-    // {
-    //     display_raw_frame(0xffff);
-    //     delay_ms(100);
-    //     display_raw_frame(0x0000);
-    //     delay_ms(100);
-    // }
-
     while(1)
     {    
         // Relay buttons
@@ -39,8 +22,8 @@ void main(void)
         // if (btn_is_pressed(ANT) || btn_is_down(ANT)) ant_hold();
 
         // remap ANT onto POWER
-        if (btn_is_pressed(POWER) || btn_is_down(POWER)) power_hold();
-        // if (btn_is_pressed(POWER) || btn_is_down(POWER)) ant_hold();
+        // if (btn_is_pressed(POWER) || btn_is_down(POWER)) power_hold();
+        if (btn_is_pressed(POWER) || btn_is_down(POWER)) ant_hold();
     }
 }
 
