7c721074991c30db2a38d2cd3619b8a6d4704175
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-04-07 06:55:51 -0400

    Added critical section macros to replace di() and ei()

--- src/interrupt.h
+++ src/interrupt.h
@@ -3,7 +3,11 @@
 
 /* ************************************************************************** */
 
-// Interrupt control functions
+// 
 extern void interrupt_init(void);
+
+// Macros to control interrupt system
+#define begin_critical_section() (INTCON0bits.GIE = 0)
+#define end_critical_section() (INTCON0bits.GIE = 1)
 
 #endif
