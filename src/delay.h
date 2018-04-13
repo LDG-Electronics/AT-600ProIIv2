36c5dc06334510f6e492ed5613fe350bd81cff89
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-04-13 00:21:32 -0400

    Added calibration data and descriptions to delay.h

--- src/delay.h
+++ src/delay.h
@@ -5,10 +5,17 @@
 
 extern void delay_init(void);
 
-// Actual delay is roughly (n + 2) useconds
+// delay_us() uses timer0 to wait n microseconds, to an accuracy of +-2%
 extern void delay_us(uint16_t useconds);
 
-// Actual delay is roughly (1000n + 5) useconds
+/*  delay_ms() uses the system tick to wait between n-1 and n milliseconds.
+    This error is caused by the 1ms resolution of the systick. delay_ms() can
+    start anywhere in the 'current' ms. Calling delay_ms() with arguments less
+    than 10 can result in ~(1/(n+1))% error, as the jitter becomes a larger and
+    larger fraction of the desired wait time.
+
+    If a high-accuracy delay is required, please use delay_us() instead.
+*/
 extern void delay_ms(uint16_t mseconds);
 
 #endif
