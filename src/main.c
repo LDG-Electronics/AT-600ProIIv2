07b4b53e82a1e36c4838df9dd3931edcabc31d54
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-06-10 19:52:04 -0400

    Removed btn_is_pressed() calls from mainloop

--- src/main.c
+++ src/main.c
@@ -11,16 +11,15 @@ void main(void)
     while(1)
     {
         // Relay buttons
-        if (btn_is_pressed(CUP) || btn_is_down(CUP)) relay_button_hold();
-        if (btn_is_pressed(CDN) || btn_is_down(CDN)) relay_button_hold();
-        if (btn_is_pressed(LUP) || btn_is_down(LUP)) relay_button_hold();
-        if (btn_is_pressed(LDN) || btn_is_down(LDN)) relay_button_hold();
+        if (check_multiple_buttons(&btn_is_down, 4, CUP, CDN, LUP, LDN)) {
+            relay_button_hold();
+        }
 
         // Other buttons
-        if (btn_is_pressed(FUNC) || btn_is_down(FUNC)) func_hold();
-        if (btn_is_pressed(TUNE) || btn_is_down(TUNE)) tune_hold();
-        if (btn_is_pressed(ANT) || btn_is_down(ANT)) ant_hold();
-        if (btn_is_pressed(POWER) || btn_is_down(POWER)) power_hold();
+        if (btn_is_down(FUNC)) func_hold();
+        if (btn_is_down(TUNE)) tune_hold();
+        if (btn_is_down(ANT)) ant_hold();
+        if (btn_is_down(POWER)) power_hold();
 
         system_idle_block();
     }
