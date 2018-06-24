2a460f30924f09fde8301de4a3e4a2b07fec500c
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-06-24 19:24:33 -0400

    Corrected errors in memory.h

--- src/memory.h
+++ src/memory.h
@@ -3,10 +3,13 @@
 
 /* ************************************************************************** */
 
-// Converts a traditional style perioud into an address in memory.
-extern uint24_t convert_memory_address(uint16_t period);
+// Converts a frequency into an address in memory.
+extern uint24_t convert_memory_address(uint16_t frequency);
 
+// Store the current relay settings at the provided address
 extern void memory_store(uint24_t address);
+
+// Recall relay settings from the provided address
 extern uint32_t memory_recall(uint24_t address);
 
 #endif
