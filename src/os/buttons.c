9e9fd25a6d9a195bc3894ca2911b1b732d731586
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-05 18:18:56 -0400

    Rewrite of button driver

--- src/os/buttons.c
+++ src/os/buttons.c
@@ -14,21 +14,19 @@ volatile uint8_t buttons[NUMBER_OF_BUTTONS];
     'Ultimate Debouncer', found at Hackaday.
     
     The general idea is to use an interrupt routine that polls the physical
-    buttons at a particular rate.  A history of button state is stored in a 
-    variable by bit shifting left one and setting the least-significant bit 
+    buttons at a particular rate.  A history of button state is stored in a
+    variable by bit shifting left one and setting the least-significant bit
     to the polled bit.
-    
+
     These checker functions compare the button history against a bit mask to
     simultaneously debounce and determine one of 5 different states.
 */
 
 /* ************************************************************************** */
 
-void buttons_init(void)
-{
+void buttons_init(void) {
     // Initialize the button history array
-    for (uint8_t i = 0; i < NUMBER_OF_BUTTONS; i++)
-    {
+    for (uint8_t i = 0; i < NUMBER_OF_BUTTONS; i++) {
         buttons[i] = 0;
     }
 
@@ -41,7 +39,7 @@ void buttons_init(void)
     timer6_period_set(0xF9);
 
     timer6_interrupt_enable();
-    
+
     timer6_start();
 }
 
@@ -60,62 +58,52 @@ void buttons_init(void)
     Then the current state is read with the macro XXXX_BUTTON_PIN, and bitwise
     OR'd into the least significant bit.
 */
-void __interrupt(irq(TMR6), high_priority) button_subsystem_ISR(void)
-{
+
+#define check_single_button(buttonIndex, buttonPin)                            \
+    do {                                                                       \
+        buttons[buttonIndex] <<= 1;                                            \
+        buttons[buttonIndex] |= buttonPin;                                     \
+    } while (0)
+
+void __interrupt(irq(TMR6), high_priority) button_subsystem_ISR(void) {
     timer6_IF_clear();
 
-    // Grab current state of each button
-    buttons[TUNE] <<= 1;
-    buttons[TUNE] |= TUNE_BUTTON_PIN;
-    
-    buttons[FUNC] <<= 1;
-    buttons[FUNC] |= FUNC_BUTTON_PIN;
-    
-    buttons[CUP] <<= 1;
-    buttons[CUP] |= CUP_BUTTON_PIN;
-    
-    buttons[CDN] <<= 1;
-    buttons[CDN] |= CDN_BUTTON_PIN;
-    
-    buttons[LUP] <<= 1;
-    buttons[LUP] |= LUP_BUTTON_PIN;
-    
-    buttons[LDN] <<= 1;
-    buttons[LDN] |= LDN_BUTTON_PIN;
-    
-    buttons[ANT] <<= 1;
-    buttons[ANT] |= ANT_BUTTON_PIN;
+    // TODO: how can I move this responsibility out of the os directory?
 
-    buttons[POWER] <<= 1;
-    buttons[POWER] |= POWER_BUTTON_PIN;
+    // Grab current state of each button
+    check_single_button(TUNE, TUNE_BUTTON_PIN);
+    check_single_button(FUNC, FUNC_BUTTON_PIN);
+    check_single_button(CUP, CUP_BUTTON_PIN);
+    check_single_button(CDN, CDN_BUTTON_PIN);
+    check_single_button(LUP, LUP_BUTTON_PIN);
+    check_single_button(LDN, LDN_BUTTON_PIN);
+    check_single_button(ANT, ANT_BUTTON_PIN);
+    check_single_button(POWER, POWER_BUTTON_PIN);
 }
 
 /* -------------------------------------------------------------------------- */
 
 // Returns 1 if any button is currently down
-uint8_t get_buttons(void)
-{
-    for (uint8_t i = 0; i < NUMBER_OF_BUTTONS; i++)
-    {
-        if(btn_is_down(i)) return 1;
+uint8_t get_buttons(void) {
+    for (uint8_t i = 0; i < NUMBER_OF_BUTTONS; i++) {
+        if (btn_is_down(i))
+            return 1;
     }
-    
+
     return 0;
 }
 
 /* -------------------------------------------------------------------------- */
 
 // Returns a 1 if any of the listed buttons is experiencing the specified state
-uint8_t check_multiple_buttons(button_check_t btn_is_xxx, uint8_t numberOfButtons, ...)
-{
+uint8_t check_multiple_buttons(button_check_t btn_is_xxx,
+                               uint8_t numberOfButtons, ...) {
     va_list ap;
 
     va_start(ap, numberOfButtons);
 
-    for(uint8_t i = 0; i < numberOfButtons; i++)
-    {
-        if(btn_is_xxx(va_arg(ap, buttonName_t)))
-        {
+    for (uint8_t i = 0; i < numberOfButtons; i++) {
+        if (btn_is_xxx(va_arg(ap, buttonName_t))) {
             va_end(list);
             return 1;
         }
@@ -125,60 +113,54 @@ uint8_t check_multiple_buttons(button_check_t btn_is_xxx, uint8_t numberOfButton
     return 0;
 }
 
+/* -------------------------------------------------------------------------- */
+
+// Returns 1 if buttons entire history is 'down', aka button is being held.
+uint8_t btn_is_down(buttonName_t buttonName) {
+    return (buttons[buttonName] == 0b11111111);
+}
+
+// Returns 1 if buttons entire history is 'up', aka button is NOT held.
+uint8_t btn_is_up(buttonName_t buttonName) {
+    return (buttons[buttonName] == 0b00000000);
+}
+
+/*  Mask out the middle three 'bouncy' bits, and compare the result with the
+    'pressed' binary literal.  This evaluates true if the button was
+    previously 'up', bounces in the middle, and has three continuous polls
+    of 'down'.
+
+    If the above conditions are met, we set the history to all down, so
+    another call to this function won't falsely detect another rising edge.
+    Then, return 1 for true.
+
+    If the conditions aren't met, return 0 for false. (Duh.)
+*/
 // Returns 1 if rising edge is detected
-uint8_t btn_is_pressed(buttonName_t buttonName)
-{
-    /*  Mask out the middle three 'bouncy' bits, and compare the result with the
-        'pressed' binary literal.  This evaluates true if the button was
-        previously 'up', bounces in the middle, and has three continuous polls of
-        'down'.
-        
-        If the above conditions are met, we set the history to all down, so
-        another call to this function won't falsely detect another rising edge. 
-        Then, return 1 for true.
-        
-        If the conditions aren't met, return 0 for false. (Duh.)
-    */
-    
-    if ((buttons[buttonName] & DEBOUNCE_MASK) == 0b00000111)
-    {
+uint8_t btn_is_pressed(buttonName_t buttonName) {
+    if ((buttons[buttonName] & DEBOUNCE_MASK) == 0b00000111) {
         buttons[buttonName] = 0b11111111;
         return 1;
     }
     return 0;
 }
 
+/*  Mask out the middle three 'bouncy' bits, and compare the result with the
+    'released' binary literal.  This evaluates true if the button was
+    previously 'down', bounces in the middle, and has three continuous polls
+    of 'up'.
+
+    If the above conditions are met, we set the history to all up, so
+    another call to this function won't falsely detect another falling edge.
+    Then, return 1 for true.
+
+    If the conditions aren't met, return 0 for false. (Duh.)
+*/
 // Returns 1 if falling edge is detected
-uint8_t btn_is_released(buttonName_t buttonName)
-{
-    /*  Mask out the middle three 'bouncy' bits, and compare the result with the
-        'released' binary literal.  This evaluates true if the button was
-        previously 'down', bounces in the middle, and has three continuous polls of
-        'up'.
-        
-        If the above conditions are met, we set the history to all up, so
-        another call to this function won't falsely detect another falling edge.
-        Then, return 1 for true.
-        
-        If the conditions aren't met, return 0 for false. (Duh.)
-    */
-    
-    if ((buttons[buttonName] & DEBOUNCE_MASK) == 0b11000000)
-    {
+uint8_t btn_is_released(buttonName_t buttonName) {
+    if ((buttons[buttonName] & DEBOUNCE_MASK) == 0b11000000) {
         buttons[buttonName] = 0b00000000;
         return 1;
     }
     return 0;
-}
-
-// Returns 1 if buttons entire history is 'down', aka button is being held.
-uint8_t btn_is_down(buttonName_t buttonName)
-{
-    return (buttons[buttonName] == 0b11111111);
-}
-
-// Returns 1 if buttons entire history is 'up', aka button is NOT held.
-uint8_t btn_is_up(buttonName_t buttonName)
-{
-    return (buttons[buttonName] == 0b00000000);
 }
--- src/os/buttons.h
+++ src/os/buttons.h
@@ -61,16 +61,16 @@ extern uint8_t get_buttons(void);
 extern uint8_t check_multiple_buttons(button_check_t btn_is_xxx,
                                       uint8_t numberOfButtons, ...);
 
-// Returns 1 if the specified button is experiencing a falling edge
-extern uint8_t btn_is_pressed(buttonName_t buttonName);
-
-// Returns 1 if the specified button is experiencing a rising edge
-extern uint8_t btn_is_released(buttonName_t buttonName);
-
 // Returns 1 if the specified button has been down for 8 samples
 extern uint8_t btn_is_down(buttonName_t buttonName);
 
 // Returns 1 if the specified button has been up for 8 samples
 extern uint8_t btn_is_up(buttonName_t buttonName);
+
+// Returns 1 if the specified button is experiencing a falling edge
+extern uint8_t btn_is_pressed(buttonName_t buttonName);
+
+// Returns 1 if the specified button is experiencing a rising edge
+extern uint8_t btn_is_released(buttonName_t buttonName);
 
 #endif
