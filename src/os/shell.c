37e5ff90dbaf6b89b718c6faa6024abf7c15933e
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-06 13:06:01 -0400

    Major rearrangement of shell driver

--- src/os/shell.c
+++ src/os/shell.c
@@ -1,214 +1,264 @@
 #include "../includes.h"
+#include "shell_keycodes.h"
 
 /* ************************************************************************** */
 
 // This structure holds the data for every command registered on the shell
 struct shell_command_entry {
-	shell_program_t shell_program;
-	const char * shell_command_string;
+    shell_program_t shell_program;
+    const char *shell_command_string;
 };
 
 /*	This structure array contains the available commands and they associated
-  	function entry point, other data required by the commands may be added to
-  	this structure
+	function entry point, other data required by the commands may be added to
+	this structure
 */
 struct shell_command_entry list[CONFIG_SHELL_MAX_COMMANDS];
 
-/* 	This array of pointers to characters holds the addresses of the begining of
-  	the parameter strings passed to the programs
-*/
+/* -------------------------------------------------------------------------- */
+
+typedef struct {
+    shell_program_t callback;
+    const char *command;
+} shell_entry_t;
+
+typedef struct commands {
+    shell_entry_t list[CONFIG_SHELL_MAX_COMMANDS];
+    uint8_t numOfRegisteredCommands;
+} command_list_t;
+
+command_list_t commands;
+
+void init_shell_commands(void) {
+    for (uint8_t i = 0; i < SHELL_BUFFER_LENGTH; i++) {
+        commands.list[i].callback = NULL;
+        commands.list[i].command = NULL;
+    }
+    commands.numOfRegisteredCommands = 0;
+}
+
+/* -------------------------------------------------------------------------- */
+
 char *argv_list[CONFIG_SHELL_MAX_COMMAND_ARGS];
 
 char shell_rx_buffer[CONFIG_SHELL_MAX_INPUT];
 
+/* -------------------------------------------------------------------------- */
+
 typedef struct {
-	char buffer[CONFIG_SHELL_MAX_INPUT];
-	uint8_t length;
+    char buffer[SHELL_BUFFER_LENGTH];
+    uint8_t length;
+    uint8_t cursorLocation;
 } shell_buffer_t;
 
+shell_buffer_t shellBuffer;
+
+void clear_shell_buffer(shell_buffer_t *ptr) {
+    for (uint8_t i = 0; i < SHELL_BUFFER_LENGTH; i++) {
+        ptr->buffer[i] = 0;
+    }
+    ptr->length = 0;
+    ptr->cursorLocation = 0;
+}
 
 /* ************************************************************************** */
 
-// forward declarations
-static int shell_parse(char * buf, char** argv, unsigned short maxargs);
-/* ************************************************************************** */
+void shell_init(void) {
+    for (uint8_t i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
+        list[i].shell_program = 0;
+        list[i].shell_command_string = 0;
+    }
 
-void shell_prompt(void)
-{
-	print(SHELL_PROMPT_STRING);
+    init_shell_commands();
+    clear_shell_buffer(&shellBuffer);
+
+    println(SHELL_VERSION_STRING);
+
+    print(SHELL_PROMPT_STRING);
 }
 
-void shell_init(void)
-{
-	for (uint8_t i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
-		list[i].shell_program = 0;
-		list[i].shell_command_string = 0;
-	}
+bool shell_register(shell_program_t program, const char *string) {
+    unsigned char i;
 
-	println(SHELL_VERSION_STRING);
-
-	shell_prompt();
-}
-
-bool shell_register(shell_program_t program, const char *string)
-{
-	unsigned char i;
-
-	for (i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
-		if (list[i].shell_program != 0 || list[i].shell_command_string != 0)
-			continue;
-		list[i].shell_program = program;
-		list[i].shell_command_string = string;
-		return true;
-	}
-	return false;
+    for (i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
+        if (list[i].shell_program != 0 || list[i].shell_command_string != 0)
+            continue;
+        list[i].shell_program = program;
+        list[i].shell_command_string = string;
+        return true;
+    }
+    return false;
 }
 
 /* -------------------------------------------------------------------------- */
 
-static int shell_parse(char * buf, char ** argv, unsigned short maxargs)
-{
-	int i = 0;
-	int argc = 0;
-	int length = strlen(buf) + 1; //String length to parse = strlen + 1
-	char toggle = 0;
+static int shell_parse(char *buf, char **argv, unsigned short maxargs) {
+    int i = 0;
+    int argc = 0;
+    int length = strlen(buf) + 1; // String length to parse = strlen + 1
+    char toggle = 0;
 
-	argv[argc] = &buf[0];
+    argv[argc] = &buf[0];
 
-	for (i = 0; i < length && argc < maxargs; i++) {
-		switch (buf[i]) {	
-			case '\0': // String terminator means at least one arg
-				i = length;
-				argc++;
-				break;
-			case '\"': // Check for double quotes for strings as parameters
-				if (toggle == 0) {
-					toggle = 1;
-					buf[i] = '\0';
-					argv[argc] = &buf[i + 1];
-				} else {
-					toggle = 0;
-					buf[i] = '\0';
-				}
-				break;
-			case ' ':
-				if (toggle == 0) {
-					buf[i] = '\0';
-					argc++;
-					argv[argc] = &buf[i + 1];
-				}
-				break;
-		}
-	}
-	return argc;
+    for (i = 0; i < length && argc < maxargs; i++) {
+        switch (buf[i]) {
+        case '\0': // String terminator means at least one arg
+            i = length;
+            argc++;
+            break;
+        case '\"': // Check for double quotes for strings as parameters
+            if (toggle == 0) {
+                toggle = 1;
+                buf[i] = '\0';
+                argv[argc] = &buf[i + 1];
+            } else {
+                toggle = 0;
+                buf[i] = '\0';
+            }
+            break;
+        case ' ':
+            if (toggle == 0) {
+                buf[i] = '\0';
+                argc++;
+                argv[argc] = &buf[i + 1];
+            }
+            break;
+        }
+    }
+    return argc;
 }
 
-void shell_process(void)
-{
-	unsigned int i = 0;
-	unsigned int retval = 0;
-	int argc = 0;
+void shell_process(void) {
+    uint8_t i = 0;
+    int retval = 0;
+    int argc = 0;
 
-	argc = shell_parse(shell_rx_buffer, argv_list, CONFIG_SHELL_MAX_COMMAND_ARGS);
-	// sequential search on command table
-	for (i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
-		if (list[i].shell_program == 0)
-			continue;
-		// If string matches one on the list
-		if (!strcmp(argv_list[0], list[i].shell_command_string)) {
-			// Run the appropiate function
-			retval = list[i].shell_program(argc, argv_list);
-			return;
-		}
-	}	
-	printf("No command '%s' found.", argv_list[0]);
+    argc =
+        shell_parse(shell_rx_buffer, argv_list, CONFIG_SHELL_MAX_COMMAND_ARGS);
+
+    // sequential search on command table
+    for (i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
+        if (list[i].shell_program == 0)
+            continue;
+
+        // If string matches one on the list
+        if (!strcmp(argv_list[0], list[i].shell_command_string)) {
+            // Run the appropiate function
+            retval = list[i].shell_program(argc, argv_list);
+
+            // check retval for... what?
+
+            return;
+        }
+    }
+    printf("No command '%s' found.", argv_list[0]);
 }
 
-void shell_backspace(void)
-{
-	putch(SHELL_ASCII_BS);
-	putch(SHELL_ASCII_SP);
-	putch(SHELL_ASCII_BS);
-}
+void shell_update(void) {
+    static char prevChar = 0;
 
-void shell_update(void)
-{
-	static char prevChar;
-	char currentChar = 0;
+    char currentChar = getch();
 
-	UART2_getc(&currentChar);
-	prevChar = currentChar;
+    // return early if we haven't rx'd a character
+    if (currentChar == KEY_NUL)
+        return;
 
-	// Number of characters written to buffer (this should be static var)
-	static unsigned short count = 0;
+    if (prevChar == KEY_ESC) {
+        if (currentChar == '[') {
+            return;
+        }
+    }
 
-	// return 
-	if (currentChar == NULL) return;
+    if (prevChar == '[') {
+        if (currentChar == KEY_UP) {
+            println("UP");
+            return;
+        }
+        if (currentChar == KEY_DOWN) {
+            println("DOWN");
+            return;
+        }
+        if (currentChar == KEY_RIGHT) {
+            println("RIGHT");
+            return;
+        }
+        if (currentChar == KEY_LEFT) {
+            // putch(KEY_ESC);
+            // putch(KEY_BS);
+            print("\[1D");
+            return;
+        }
+    }
 
-	// control characters
-	if (iscntrl(currentChar)){
-		if ((currentChar == SHELL_ASCII_ESC) || (currentChar == SHELL_ASCII_ESC)) {
-			println("esc");
-			return;
-		}
+    prevChar = currentChar;
 
-		if (currentChar == SHELL_ASCII_DEL) {
-			println("del");
-			return;
-		}
+    // Number of characters written to buffer (this should be static var)
+    static unsigned short count = 0;
 
-		if (currentChar == SHELL_ASCII_HT) {
-			println("tab");
-			return;
-		}
+    // process control characters
+    if (iscntrl(currentChar)) {
+        if (currentChar == KEY_ESC) {
+            // println(&currentChar);
+            return;
+        }
 
-		if (currentChar == SHELL_ASCII_CR) {
-			shell_rx_buffer[count] = '\0';
-			println(""); 
-			if (count > 0) {
-				shell_process();
+        if (currentChar == SHELL_ASCII_DEL) {
+            // println("del");
+            return;
+        }
 
-				count = 0;
-			} else {
-				println("command not found");
-			}
-			println("");
-			shell_prompt();
-			return;
-		}
+        if (currentChar == SHELL_ASCII_HT) {
+            // println("tab");
+            return;
+        }
 
-		if (currentChar == SHELL_ASCII_BS) {
-			if (count > 0) {
-				count--;
-				shell_backspace();
-			} else {
-				putch(SHELL_ASCII_BEL);
-			}
-			return;
-		}
-	}
+        if (currentChar == KEY_BS) {
+            if (count > 0) {
+                count--;
+                putch(KEY_BS);
+				putch(KEY_SP);
+				putch(KEY_BS);
+            } else {
+                putch(SHELL_ASCII_BEL);
+            }
+            return;
+        }
 
-	// printable characters
-	if (isprint(currentChar)){
-		if (count < CONFIG_SHELL_MAX_INPUT) {
-			shell_rx_buffer[count] = currentChar;
-			putch(currentChar);
-			count++;
-		}
-		return;
-	}
+        if (currentChar == SHELL_ASCII_CR) {
+            shell_rx_buffer[count] = '\0';
+            println("");
+            if (count > 0) {
+                shell_process();
+
+                count = 0;
+            } else {
+                println("command not found");
+            }
+            println("");
+            print(SHELL_PROMPT_STRING);
+            return;
+        }
+    }
+
+    // process printable characters
+    if (isprint(currentChar)) {
+        if (count < CONFIG_SHELL_MAX_INPUT) {
+            shell_rx_buffer[count] = currentChar;
+            putch(currentChar);
+            count++;
+        }
+        return;
+    }
 }
 
 /* -------------------------------------------------------------------------- */
 
-void shell_print_commands(void)
-{
-	unsigned char i;
+void shell_print_commands(void) {
+    unsigned char i;
 
-	for (i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
-		if (list[i].shell_program != 0 || list[i].shell_command_string != 0) {
-			println(list[i].shell_command_string);
-		}
-	}
+    for (i = 0; i < CONFIG_SHELL_MAX_COMMANDS; i++) {
+        if (list[i].shell_program != 0 || list[i].shell_command_string != 0) {
+            println(list[i].shell_command_string);
+        }
+    }
 }
-
