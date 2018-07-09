d3cf01999d0be74673f216f60c9b811cd187cdb7
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-09 19:36:36 -0400

    Refactored process_shell_command() to make modifications easier

--- src/os/shell/shell_command_processing.c
+++ src/os/shell/shell_command_processing.c
@@ -17,66 +17,30 @@ int8_t find_matching_command(char *string) {
 }
 
 void process_shell_command(void) {
-    if (shell.length == 0) {
-        return;
-    }
-
     int argc = 0;
     int length = strlen(shell.buffer) + 1;
-    char toggle = 0;
 
     char *argv_list[CONFIG_SHELL_MAX_COMMAND_ARGS];
 
     argv_list[argc] = &shell.buffer[0];
 
-    /* //TODO: this wrongly parses multiple spaces in a command
-        input "test 1 2 3"
-        Received 4 arguments for test command
-        0 - "test" [len:4]
-        1 - "1" [len:1]
-        2 - "2" [len:1]
-        3 - "3" [len:1]
+    char currentChar = 0;
+    char prevChar = 0;
 
-        input "test 1  2"
-        Received 4 arguments for test command
-        0 - "test" [len:4]
-        1 - "1" [len:1]
-        2 - "" [len:0] //* <- not correct
-        3 - "2" [len:1]
+    for (uint8_t i = 0; i < length; i++) {
+        currentChar = shell.buffer[i];
 
-        input: "test   "
-        Received 4 arguments for test command
-        0 - "test" [len:4]
-        1 - "" [len:0] //* <- not correct
-        2 - "" [len:0] //* <- not correct
-        3 - "" [len:0] //* <- not correct
-    */
-
-    for (uint8_t i = 0; i < length && argc < CONFIG_SHELL_MAX_COMMAND_ARGS;
-         i++) {
-        switch (shell.buffer[i]) {
-        case '\0': // String terminator means at least one arg
+        if (currentChar == '\0') {
             i = length;
             argc++;
-            break;
-        case '\"': // Check for double quotes for strings as parameters
-            if (toggle == 0) {
-                toggle = 1;
-                shell.buffer[i] = '\0';
-                argv_list[argc] = &shell.buffer[i + 1];
-            } else {
-                toggle = 0;
-                shell.buffer[i] = '\0';
-            }
-            break;
-        case ' ': // Command arguments are separated by spaces
-            if (toggle == 0) {
-                shell.buffer[i] = '\0';
-                argc++;
-                argv_list[argc] = &shell.buffer[i + 1];
-            }
-            break;
         }
+        if (currentChar == ' ') {
+            shell.buffer[i] = '\0';
+            argc++;
+            argv_list[argc] = &shell.buffer[i + 1];
+        }
+
+        prevChar = shell.buffer[i];
     }
 
     int8_t command = find_matching_command(argv_list[0]);
