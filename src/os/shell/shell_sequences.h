c90033d7ecad9998a4af411ea3396d549cb52c97
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-09 23:17:42 -0400

    Removed deprecated stuff from shell_sequences.h

--- src/os/shell/shell_sequences.h
+++ src/os/shell/shell_sequences.h
@@ -3,15 +3,6 @@
 
 /* ************************************************************************** */
 
-// This macro can disable all the costly key name debug operations
-// #define set_key_name(string)
-
-// Call this every time we resolve a control keystroke
-extern void set_key_name(const char * string);
-
-/* -------------------------------------------------------------------------- */
-
-extern void process_control_character(char currentChar);
 extern void process_escape_sequence(char currentChar);
 
 #endif
