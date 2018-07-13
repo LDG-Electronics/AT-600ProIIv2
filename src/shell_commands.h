6af6e9c30d740152c29d1692a7db37f8e5ae54a6
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-13 17:23:35 -0400

    Function definitions for commands don't need to be in the header

--- src/shell_commands.h
+++ src/shell_commands.h
@@ -3,28 +3,6 @@
 
 /* ************************************************************************** */
 
-// Shell command standard return values
-#define SHELL_RET_SUCCESS 0
-#define SHELL_RET_FAILURE 1
-
-/* ************************************************************************** */
-
 extern void register_all_shell_commands(void);
-
-/* ************************************************************************** */
-
-// from shell.c
-extern int shell_help(int argc, char **argv);
-extern int shell_arg_test(int argc, char **argv);
-
-// general purpose parameter touching
-extern int shell_get_param(int argc, char **argv);
-extern int shell_set_param(int argc, char **argv);
-
-// from RF_sensor.c
-extern int shell_get_RF(int argc, char **argv);
-
-// from display.c
-extern int shell_show_bargraphs(int argc, char **argv);
 
 #endif
