d29fff366797feea7203e8e842599ca68fbdf575
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-13 17:40:18 -0400

    Most of the contents of ui.c no longer need to be extern

--- src/ui.h
+++ src/ui.h
@@ -2,16 +2,6 @@
 #define _MENUS_H_
 
 /* ************************************************************************** */
-// Nested state machine handlers for front panel buttons
-
-// relay increment buttons
-extern void relay_button_hold(void);
-
-// other buttons
-extern void tune_hold(void);
-extern void func_hold(void);
-extern void ant_hold(void);
-extern void power_hold(void);
 
 extern void main_menu(void);
 
