085c0cb83a0f062dc4db2f3ff54ae8045c96ae37
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-17 16:36:55 -0400

    Applied formatting to adc driver

--- src/peripherals/adc.h
+++ src/peripherals/adc.h
@@ -7,10 +7,10 @@
     
     Vdd and Vss are the reference voltages, acquisition time is set to as
     slow as possible, the result is right justified.
-    
+
     Eventually, adc_init could allow for reconfiguration at runtime, but
     currently there's nothing worth changing.
-    
+
     adc_measure() initiates a/d conversion on the selected channel, waits until
     the conversion finishes, then returns the result.
 */
