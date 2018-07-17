247bd934922772e486cb29a660692448fd1966a2
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-17 10:46:42 -0400

    Implemented Ian's suggestions RE: LOG_MACRO formatting

--- src/peripherals/reset.c
+++ src/peripherals/reset.c
@@ -3,26 +3,32 @@
 
 /* ************************************************************************** */
 
-void reset_vector_handler(void)
-{
-    if(PCON0bits.STKOVF == 1) {
-        LOG_FATAL(println("\r\n\r\n>>> Stack Overflow <<<"););
-        while(1); // trap
+void reset_vector_handler(void) {
+    if (PCON0bits.STKOVF == 1) {
+        LOG_FATAL({ println("\r\n\r\n>>> Stack Overflow <<<"); });
+        while (1) {
+            // trap
+        }
     }
-    
-    if(PCON0bits.STKUNF == 1) {
-        LOG_FATAL(println("\r\n\r\n>>> Stack Underflow <<<"););
-        while(1); // trap
+
+    if (PCON0bits.STKUNF == 1) {
+        LOG_FATAL({ println("\r\n\r\n>>> Stack Underflow <<<"); });
+        while (1) {
+            // trap
+        }
     }
-    
-    // if(PCON0bits.RMCLR == 0) {
-        // LOG_FATAL(println("\r\n\r\n>>> MCLR RESET <<<"););
-        // while(1); // trap
-    // }
-    
-    // if(PCON0bits.RI == 0) {
-        // LOG_FATAL(println("\r\n\r\n>>> RESET <<<"););
-        // while(1); // trap
-    // }
-    
+
+    if (PCON0bits.RMCLR == 0) {
+        LOG_FATAL(println("\r\n\r\n>>> MCLR RESET <<<"););
+        while (1) {
+            // trap
+        }
+    }
+
+    if (PCON0bits.RI == 0) {
+        LOG_FATAL(println("\r\n\r\n>>> RESET <<<"););
+        while (1) {
+            // trap
+        }
+    }
 }
