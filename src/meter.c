ee5c1005bd5b95e0d97346cae53db70b917c08af
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-18 13:36:13 -0400

    Rearranged meter.c's internal data structure

--- src/meter.c
+++ src/meter.c
@@ -8,12 +8,17 @@
 // The meter update can contain zeros, so the terminator has to be something
 // other than null.
 #define METER_COMMS_TERMINATOR '\e'
+
 // Apparently, the magic string for syncing is 15 zeros plus AzAz. This was
 // chosen by Russ during the High-Power Meter development, and can't be changed.
 #define METER_SYNC_STRING "000000000000000AzAz"
 
-uint8_t meter_update_status = 0;
-char meter_update_buffer[9];
+struct {
+    char packetBuffer[9];
+    unsigned active : 1;
+} meter;
+
+/* ************************************************************************** */
 
 void meter_init(void) {
     // PPS Setup
@@ -23,9 +28,9 @@ void meter_init(void) {
     UART1_init(_38400);
 
     // The last three bytes of the buffer never change, so init them here
-    meter_update_buffer[6] = ';';
-    meter_update_buffer[7] = ';';
-    meter_update_buffer[8] = METER_COMMS_TERMINATOR;
+    meter.packetBuffer[6] = ';';
+    meter.packetBuffer[7] = ';';
+    meter.packetBuffer[8] = METER_COMMS_TERMINATOR;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -38,9 +43,9 @@ void check_for_meter_sync(void) {
     } else if (data == 'Z') {
         UART1_tx_string(METER_SYNC_STRING, '\0');
     } else if (data == 'S') {
-        meter_update_status = 1;
+        meter.active = 1;
     } else if (data == 'X') {
-        meter_update_status = 0;
+        meter.active = 0;
     }
 }
 
@@ -49,14 +54,14 @@ void send_meter_update(void) {
     uint16_t tempREV = adc_measure(1) >> 2;
     uint16_t tempPeriod = 0xffff;
 
-    meter_update_buffer[0] = (uint8_t)(tempFWD >> 8);
-    meter_update_buffer[1] = (uint8_t)(tempFWD & 0xff);
-    meter_update_buffer[2] = (uint8_t)(tempREV >> 8);
-    meter_update_buffer[3] = (uint8_t)(tempREV & 0xff);
-    meter_update_buffer[4] = (uint8_t)(tempPeriod >> 8);
-    meter_update_buffer[5] = (uint8_t)(tempPeriod & 0xff);
+    meter.packetBuffer[0] = (uint8_t)(tempFWD >> 8);
+    meter.packetBuffer[1] = (uint8_t)(tempFWD & 0xff);
+    meter.packetBuffer[2] = (uint8_t)(tempREV >> 8);
+    meter.packetBuffer[3] = (uint8_t)(tempREV & 0xff);
+    meter.packetBuffer[4] = (uint8_t)(tempPeriod >> 8);
+    meter.packetBuffer[5] = (uint8_t)(tempPeriod & 0xff);
 
-    UART1_tx_string(meter_update_buffer, METER_COMMS_TERMINATOR);
+    UART1_tx_string(meter.packetBuffer, METER_COMMS_TERMINATOR);
 }
 
 #define METER_UPDATE_INTERVAL 100
@@ -65,7 +70,7 @@ void attempt_meter_update(void) {
 
     check_for_meter_sync();
 
-    if (meter_update_status == 1) {
+    if (meter.active == 1) {
         system_time_t currentTime = systick_read();
 
         if (currentTime >= nextUpdateTime) {
