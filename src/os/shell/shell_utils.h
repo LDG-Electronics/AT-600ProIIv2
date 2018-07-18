b8d3475030eb212e3b0e5e8ac2b117b29d0e6c63
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-07-18 14:17:08 -0400

    Improved verbosity of shell_utils.h

--- src/os/shell/shell_utils.h
+++ src/os/shell/shell_utils.h
@@ -5,11 +5,17 @@
 
 // terminal control escape sequences
 #define reset_text_attributes() print("\033[0m")
+
+// clear the entire screen and move the cursor to 0,0
 #define term_reset_screen() print("\033[2J")
 
+// move the cursor to the given position
 #define term_cursor_set(x, y) print("\033[##x##;##y##H")
 
+// Move the cursor left a ton. The terminal won't let it go past the edge.
 #define term_cursor_home() print("\033[100D")
+
+// Move the cursor in some direction
 #define term_cursor_up(distance) print("\033[##distance##A")
 #define term_cursor_down(distance) print("\033[##distance##B")
 #define term_cursor_right(distance) print("\033[##distance##C")
