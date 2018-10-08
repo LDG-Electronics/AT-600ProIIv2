10d371738d57d43682dd682d65753931621c0233
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-10-08 16:42:57 -0400

    Disable logging

--- src/ui.c
+++ src/ui.c
@@ -8,7 +8,7 @@
 #include "os/shell/shell.h"
 #include "os/system_time.h"
 #include "rf_sensor.h"
-static uint8_t LOG_LEVEL = L_TRACE;
+static uint8_t LOG_LEVEL = L_SILENT;
 
 /* ************************************************************************** */
 
