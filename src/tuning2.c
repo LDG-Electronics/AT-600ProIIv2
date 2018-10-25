5d637aaeb5885b6e5d87adfb6be0abad5a9fc9de
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-10-25 15:54:32 -0400

    Added stubs for tuning algo replacement

---
+++ src/tuning2.c
@@ -0,0 +1,14 @@
+#include "tuning2.h"
+#include "calibration.h"
+#include "display.h"
+#include "flags.h"
+#include "memory.h"
+#include "os/log_macros.h"
+#include "os/system_time.h"
+#include "relays.h"
+#include "rf_sensor.h"
+#include <float.h>
+static uint8_t LOG_LEVEL = L_SILENT;
+/* ************************************************************************** */
+
+void tuning2_init(void) { log_register(); }
