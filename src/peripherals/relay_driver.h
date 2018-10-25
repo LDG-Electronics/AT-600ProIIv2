1a0dd599ba0b7d07efb281f4bfd59c117f075cb4
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-10-25 15:59:50 -0400

    Moved hardware definitions from relay.h to relay_driver.h

--- src/peripherals/relay_driver.h
+++ src/peripherals/relay_driver.h
@@ -4,6 +4,46 @@
 #include <stdint.h>
 
 /* ************************************************************************** */
+/*  AT-600ProII Relay Configuration
+
+    The AT-600ProII has 7 Capacitors, 7 Inductors, and 1 HiLoZ relay.
+
+    It uses 1500 Watt non-latching, 12 volt single coil relays.  The relays are
+    controlled by a pair of daisy-chained TPIC6B595 Serial-in/Parallel-out
+    SPI controlled shift registers.
+*/
+
+// The 1500 watt relays in this product require a long debounce time
+#define RELAY_COIL_DELAY 15 // debounce time in ms
+
+#define NUM_OF_INDUCTORS 7
+#define NUM_OF_CAPACITORS 7
+
+#define MAX_INDUCTORS 127
+#define MAX_CAPACITORS 127
+
+#define MIN_INDUCTORS 0
+#define MIN_CAPACITORS 0
+
+#define NUM_OF_ANTENNA_PORTS 2
+
+/* ************************************************************************** */
+/*  relays_t is used to simplify the translation of software representation of
+    relay information into a packed bit representation that's useful for
+    communicating with the hardware.
+*/
+typedef union {
+    struct {
+        unsigned caps : 7;
+        unsigned z : 1;
+        unsigned inds : 7;
+        unsigned ant : 1; // Relay is wired backwards: the off position is ANT2
+    };
+    uint16_t bits;
+} packed_relay_bits_t;
+
+
+/* ************************************************************************** */
 
 extern void relay_driver_init(void);
 
--- src/relays.h
+++ src/relays.h
@@ -1,35 +1,18 @@
 #ifndef _RELAYS_H_
 #define _RELAYS_H_
 
+#include "peripherals/relay_driver.h"
 #include <stdint.h>
 
 /* ************************************************************************** */
-
 /*  AT-600ProII Relay Configuration
 
     The AT-600ProII has 7 Capacitors, 7 Inductors, and 1 HiLoZ relay.
 
-    It uses 1500 Watt non-latching, 12 volt single coil relays.  The relays are
-    controlled by a pair of daisy-chained TPIC6B595 Serial-in/Parallel-out
-    SPI controlled shift registers.
+    It uses 1500 Watt non-latching, 12 volt single coil relays.
 */
 
-// The 1500 watt relays in this product require a long debounce time
-#define RELAY_COIL_DELAY 15 // debounce time in ms
-
-#define NUM_OF_INDUCTORS 7
-#define NUM_OF_CAPACITORS 7
-
-#define MAX_INDUCTORS 127
-#define MAX_CAPACITORS 127
-
-#define MIN_INDUCTORS 0
-#define MIN_CAPACITORS 0
-
-#define NUM_OF_ANTENNA_PORTS 2
-
 /* ************************************************************************** */
-
 /*  relays_t is used to simplify the storage and management of relay settings.
 
 */
