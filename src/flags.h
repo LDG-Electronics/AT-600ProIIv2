5025a55091f1e9711a365b05251a941e5ff4e45f
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-10-29 16:23:30 -0400

    system_flags_t no longer needs its union field

--- src/flags.h
+++ src/flags.h
@@ -11,23 +11,19 @@
     If possible, keep this struct under 8 bits in size, because we have to
     access if from all over the place.
 */
-typedef union {
-    struct {
-        unsigned ant1Bypass : 1; // Are we in bypass mode?
-        unsigned ant2Bypass : 1; // Are we in bypass mode?
-        unsigned antenna : 1; // Which antenna is selected 
-        unsigned autoMode : 1; // Are we in semi (0) or full (1) auto mode?
-        unsigned peakMode : 1; // 
-        unsigned scaleMode : 1; // 
-        unsigned powerStatus : 1;
-        unsigned unused : 1;
-    };
-    uint8_t savedFlags;
+typedef struct {
+    unsigned ant1Bypass : 1; // Are we in bypass mode?
+    unsigned ant2Bypass : 1; // Are we in bypass mode?
+    unsigned antenna : 1;    // Which antenna is selected
+    unsigned autoMode : 1;   // Are we in semi (0) or full (1) auto mode?
+    unsigned peakMode : 1;   //
+    unsigned scaleMode : 1;  //
+    unsigned powerStatus : 1;
 } system_flags_t;
 
 /* ************************************************************************** */
 
-// Global flags 
+// Global flags
 extern system_flags_t systemFlags;
 
 /*  This is used to access the current bypass status using the same idiom as
