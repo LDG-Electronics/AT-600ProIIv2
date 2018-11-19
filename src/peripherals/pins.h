6dcc3d1c787b3c2f660acc7210041031d77f7eb5
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-11-19 02:30:01 -0500

    Porting improvements from K42 SDK

--- src/peripherals/pins.h
+++ src/peripherals/pins.h
@@ -2,56 +2,61 @@
 #define _PINS_H_
 
 /* ************************************************************************** */
+
+#define PORT(port, pin) PORT##port##bits.R##port##pin
+#define LATCH(port, pin) LAT##port##bits.LAT##port##pin
+
+/* ************************************************************************** */
 // Button stuff
-#define POWER_BUTTON_PIN !PORTAbits.RA3
-#define CDN_BUTTON_PIN !PORTAbits.RA4
-#define LUP_BUTTON_PIN !PORTAbits.RA5
-#define CUP_BUTTON_PIN !PORTBbits.RB2
-#define FUNC_BUTTON_PIN !PORTBbits.RB4
-#define LDN_BUTTON_PIN !PORTBbits.RB5
-// #define ANT_BUTTON_PIN !PORTBbits.RB6
-#define ANT_BUTTON_PIN !PORTEbits.RE2
-// #define TUNE_BUTTON_PIN !PORTBbits.RB7
-#define TUNE_BUTTON_PIN !PORTEbits.RE1
+#define POWER_BUTTON_PIN !PORT(A, 3)
+#define CDN_BUTTON_PIN !PORT(A, 4)
+#define LUP_BUTTON_PIN !PORT(A, 5)
+#define CUP_BUTTON_PIN !PORT(B, 2)
+#define FUNC_BUTTON_PIN !PORT(B, 4)
+#define LDN_BUTTON_PIN !PORT(B, 5)
+// #define ANT_BUTTON_PIN !PORT(B, 6)
+#define ANT_BUTTON_PIN !PORT(E, 2)
+// #define TUNE_BUTTON_PIN !PORT(B, 7)
+#define TUNE_BUTTON_PIN !PORT(E, 1)
 
 /* -------------------------------------------------------------------------- */
 // Front Panel LEDs
 // Bargraph bitbang SPI - Unused since the completion of the SPI driver
-#define FP_CLOCK_PIN LATAbits.LATA6
-#define FP_DATA_PIN LATCbits.LATC5
-#define FP_STROBE_PIN LATCbits.LATC4
+#define FP_CLOCK_PIN LATCH(A, 6)
+#define FP_DATA_PIN LATCH(C, 5)
+#define FP_STROBE_PIN LATCH(C, 4)
 
 // Status LED pins
-#define POWER_LED_PIN LATAbits.LATA2
-#define BYPASS_LED_PIN LATCbits.LATC3
-#define ANT_LED_PIN LATBbits.LATB1
+#define POWER_LED_PIN LATCH(A, 2)
+#define BYPASS_LED_PIN LATCH(C, 3)
+#define ANT_LED_PIN LATCH(B, 1)
 
 /* -------------------------------------------------------------------------- */
 // RF Sensor
-// #define FREQ_PIN PORTEbits.RE3 // frequency counter
-#define FREQ_PIN PORTEbits.RE0 // frequency counter
-#define FWD_PIN PORTAbits.RA0  // forward power
-#define REV_PIN PORTAbits.RA1  // reverse power
+// #define FREQ_PIN PORT(E, 3) // frequency counter
+#define FREQ_PIN PORT(E, 0) // frequency counter
+#define FWD_PIN PORT(A, 0)  // forward power
+#define REV_PIN PORT(A, 1)  // reverse power
 
 // External Interrupts
-#define RF_INT_PIN PORTBbits.RB3    // rf interrupt - unused
-#define INTERRUPT_PIN PORTBbits.RB0 // front panel button interrupt - unused
+#define RF_INT_PIN PORT(B, 3)    // rf interrupt - unused
+#define INTERRUPT_PIN PORT(B, 0) // front panel button interrupt - unused
 
 // Radio key line - used with IC-PAC or YT-PAC-1200 cable to key radio
-#define RADIO_CMD_PIN PORTAbits.RA7
+#define RADIO_CMD_PIN LATCH(A, 7)
 
 // Relay driver bitbang SPI
-#define RELAY_CLOCK_PIN LATCbits.LATC0
-#define RELAY_DATA_PIN LATCbits.LATC1
-#define RELAY_STROBE_PIN LATCbits.LATC2
+#define RELAY_CLOCK_PIN LATCH(C, 0)
+#define RELAY_DATA_PIN LATCH(C, 1)
+#define RELAY_STROBE_PIN LATCH(C, 2)
 
 // Meter port - Unused since the completion of the UART driver
-#define METER_TX_PIN LATCbits.LATC6
-#define METER_RX_PIN PORTCbits.RC7
+#define METER_TX_PIN LATCH(C, 6)
+#define METER_RX_PIN PORT(C, 7)
 
 // Serial debug
-#define DEBUG_TX_PIN LATDbits.LATD2
-#define DEBUG_RX_PIN PORTDbits.RD1
+#define DEBUG_TX_PIN LATCH(D, 2)
+#define DEBUG_RX_PIN PORT(D, 1)
 
 /* ************************************************************************** */
 
