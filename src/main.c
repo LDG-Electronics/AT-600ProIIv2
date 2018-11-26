8329a6a72dd2d352a2bec21e8dcfa927d79908db
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-11-26 03:46:16 -0500

    These warnings actually still need to be suppressed

--- src/main.c
+++ src/main.c
@@ -4,8 +4,14 @@
 
 /* ************************************************************************** */
 
+// Disable "unused variable" warning
+#pragma warning disable 1090
+
 // Disable warning spam about reset vectors
 #pragma warning disable 2020
+
+// Disable warning spam about unused functions
+#pragma warning disable 520
 
 /* ************************************************************************** */
 
