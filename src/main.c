c7e7ae836d365e4ca0f33f3eeac0d4124d6665e9
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-11-26 01:34:50 -0500

    Removed warning suppression that we don't need anymore

--- src/main.c
+++ src/main.c
@@ -2,14 +2,10 @@
 #include "peripherals/config.h"
 #include "ui.h"
 
-// Disable "unused variable" warning
-#pragma warning disable 1090
+/* ************************************************************************** */
 
 // Disable warning spam about reset vectors
 #pragma warning disable 2020
-
-// Disable warning spam about unused functions
-#pragma warning disable 520
 
 /* ************************************************************************** */
 
