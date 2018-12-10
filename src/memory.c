a5f89496faba2d563250a1a1a5ee595ed7d80d13
DaelonSuzuka <daelonsuzuka@gmail.com>
2018-12-10 18:14:53 -0500

    More formatting improvements

--- src/memory.c
+++ src/memory.c
@@ -123,11 +123,10 @@ void print_all_frequency_groups(void) {
     for (uint8_t group = 0; group < NUMBER_OF_GROUPS; group++) {
         totalSlots += group_edges[group].slots;
         frequency_group_t tempGroup = group_edges[group];
+        uint16_t groupWidth = tempGroup.end - tempGroup.start;
 
         printf("%02u", group);
-        // print("\t");
         printf(" | (%05u, %05u)", tempGroup.start, tempGroup.end);
-        uint16_t groupWidth = tempGroup.end - tempGroup.start;
         printf(" | %05u KHz", groupWidth);
         printf(" |  %u ", tempGroup.slots);
         printf(" | %2u KHz/slot", (groupWidth / tempGroup.slots));
