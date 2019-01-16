4cbf32e517beb312a190bdcb9cbd4235ee241b99
DaelonSuzuka <daelonsuzuka@gmail.com>
2019-01-16 17:05:17 -0500

    Removed sh_cal.c/h

--- src/shellcommands/sh_cal.c
+++
@@ -1,9 +0,0 @@
-#include "sh_cal.h"
-#include "../rf_sensor.h"
-
-/* -------------------------------------------------------------------------- */
-
-void calibration_packet(int argc, char **argv) {
-    //
-    print_RF_data();
-}
