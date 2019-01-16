cc583f73857c61ee46d6e0aa81d8830299b288b0
DaelonSuzuka <daelonsuzuka@gmail.com>
2019-01-16 16:48:09 -0500

    Pulled in shell command changes from AT-200ProIIv2

--- src/shellcommands/command_list.h
+++ src/shellcommands/command_list.h
@@ -3,20 +3,20 @@
 
 #include "sh_adc.h"
 #include "sh_bar.h"
-#include "sh_cal.h"
 #include "sh_eeprom.h"
 #include "sh_flash.h"
 #include "sh_logedit.h"
 #include "sh_poly.h"
 #include "sh_relays.h"
 #include "sh_romedit.h"
+#include "sh_status.h"
 #include "sh_tune.h"
 
 /* ************************************************************************** */
 
 #define SHELL_COMMANDS                                                         \
-    SH_ADC, SH_BAR, SH_CAL, SH_EEPROM, SH_FLASH, SH_LOGEDIT, SH_POLY,          \
-        SH_RELAYS, SH_ROMEDIT, SH_TUNE
+    SH_ADC, SH_BAR, SH_EEPROM, SH_FLASH, SH_LOGEDIT, SH_POLY, SH_RELAYS,       \
+        SH_ROMEDIT, SH_STATUS, SH_TUNE
 
 /* ************************************************************************** */
 
--- src/shellcommands/sh_adc.c
+++ src/shellcommands/sh_adc.c
@@ -9,40 +9,6 @@
 
 /* ************************************************************************** */
 
-// uint8_t array1[256]; // <-- compiles
-// uint8_t array2[257]; // <-- compiles
-// uint8_t array3[512]; // <-- compiles
-// uint8_t array4[1024]; // <-- compiles
-
-// void some_func(void) {
-//     uint8_t array1[256]; // <-- compiles
-//     uint8_t array2[257]; // <-- DOES NOT COMPILE
-//     uint8_t array3[512]; // <-- DOES NOT COMPILE
-//     uint8_t array4[1024]; // <-- DOES NOT COMPILE
-// }
-
-/* ************************************************************************** */
-/*  controlling the adc from the shell/python
-
-    I need to test adc settings faster than is practical by editing, building,
-    and uploading. We're going to expose ADC registers to the shell and do this
-    like a real programmer.
-
-    ADCLK - clock selection, <0...64> (FOSC/n)
-    ADPRE - precharge time, <0...8191>
-    ADACQ - acquisition time, <0...8191>
-    ADCAP - additional capacitance, <0...31> (n pF)
-
-    I also need to expiriment with sampling techniques and DSP. We're going to
-    do tests like grabbing 256 adc samples as rapidly as possible, and then
-    piping them to the shell and having the servitor draw pretty graphs.
-
-*/
-void sh_adc_select_clock(uint8_t value) { ADCLK = value; }
-void sh_adc_set_precharge_time(uint16_t value) { ADPRE = value; }
-void sh_adc_set_acquisition_time(uint16_t value) { ADACQ = value; }
-void sh_adc_set_additional_capacitance(uint8_t value) { ADCAP = value; }
-
 void sh_adc_init(void) {
     ADREFbits.NREF = 0;    // Negative Voltage Reference, set to Vss
     ADREFbits.PREF = 0b11; // Positive Voltage Reference, set to FVR
@@ -55,75 +21,142 @@ void sh_adc_init(void) {
     ADCON0bits.ON = 1; // Enable ADC peripheral
 }
 
-#define NUM_OF_TEST_SAMPLES 500
+/* -------------------------------------------------------------------------- */
+/*  controlling the adc from the shell/python
+
+    I need to test adc settings faster than is practical by editing, building,
+    and uploading. We're going to expose ADC registers to the shell and do this
+    like a real programmer.
+
+    ADCLK - clock selection, <0...64> (FOSC/n)
+    ADPRE - precharge time, <0...8191>
+    ADACQ - acquisition time, <0...8191>
+    ADCAP - additional capacitance, <0...31> (n pF)
+
+    I also need to experiment with sampling techniques and DSP. We're going to
+    do tests like grabbing 256 adc samples as rapidly as possible, and then
+    piping them to the shell and having the servitor draw pretty graphs.
+
+*/
+#define ADCLK_MAX 63
+void sh_adc_set_clock(int16_t value) {
+    if (value >= 0 && value <= ADCLK_MAX) {
+        ADCLK = value;
+        // printf("ADCLK: %u\r\n", ADCLK);
+    }
+}
+
+#define ADPRE_MAX 8191
+void sh_adc_set_precharge_time(int16_t value) {
+    if (value >= 0 && value <= ADPRE_MAX) {
+        ADPRE = value;
+        // printf("ADPRE: %u\r\n", ADPRE);
+    }
+}
+
+#define ADACQ_MAX 8191
+void sh_adc_set_acquisition_time(int16_t value) {
+    if (value >= 0 && value <= ADACQ_MAX) {
+        ADACQ = value;
+        // printf("ADACQ: %u\r\n", ADACQ);
+    }
+}
+
+#define ADCAP_MAX 31
+void sh_adc_set_additional_capacitance(int16_t value) {
+    if (value >= 0 && value <= ADCAP_MAX) {
+        ADCAP = value;
+        // printf("ADCAP: %u\r\n", ADCAP);
+    }
+}
+
+void sh_print_settings(void) {
+    printf("ADCLK: %u\r\n", ADCLK);
+    printf("ADPRE: %u\r\n", ADPRE);
+    printf("ADACQ: %u\r\n", ADACQ);
+    printf("ADCAP: %u\r\n", ADCAP);
+}
+
+/* -------------------------------------------------------------------------- */
+
+#define NUM_OF_TEST_SAMPLES 64
 uint16_t fwdArray[NUM_OF_TEST_SAMPLES];
 uint16_t revArray[NUM_OF_TEST_SAMPLES];
 
-void sh_adc_read(void) {
+void print_array(const char *label, uint16_t *array) {
+    print("\"");
+    print(label);
+    print("\"");
 
-    us_stopwatch_begin();
+    println("[");
+    print("  ");
+    for (uint16_t i = 0; i < NUM_OF_TEST_SAMPLES; i++) {
+        printf("%4u", array[i]);
+        if (i < NUM_OF_TEST_SAMPLES - 1) {
+            print(",");
+
+            if (((i + 1) % 8) == 0) {
+                println("");
+                if (i < NUM_OF_TEST_SAMPLES - 1) {
+                    print("  ");
+                }
+            }
+        }
+    }
+    println("");
+    print("]");
+}
+
+void sh_adc_read(void) {
     // Collect measurements
     for (uint16_t i = 0; i < NUM_OF_TEST_SAMPLES; i++) {
         fwdArray[i] = adc_read(0);
         revArray[i] = adc_read(1);
     }
-    us_stopwatch_println();
 
     println("{");
-    print("  \"forward\":");
-    println("[");
-    print("    ");
-    for (uint16_t i = 0; i < NUM_OF_TEST_SAMPLES; i++) {
-        printf("%4u", fwdArray[i]);
-        if (i < NUM_OF_TEST_SAMPLES - 1) {
-            print(",");
-
-            if (((i + 1) % 16) == 0) {
-                println("");
-                if (i < NUM_OF_TEST_SAMPLES - 1) {
-                    print("    ");
-                }
-            }
-        }
-    }
-    println("");
-    println("  ],");
-
-    print("  \"reverse\":");
-    println("[");
-    print("    ");
-    for (uint16_t i = 0; i < NUM_OF_TEST_SAMPLES; i++) {
-        printf("%4u", revArray[i]);
-        if (i < NUM_OF_TEST_SAMPLES - 1) {
-            print(",");
-
-            if (((i + 1) % 16) == 0) {
-                println("");
-                if (i < NUM_OF_TEST_SAMPLES - 1) {
-                    print("    ");
-                }
-            }
-        }
-    }
-    println("");
-    println("  ]");
+    print_array("forward", &fwdArray);
+    println(",");
+    print_array("reverse", &revArray);
     println("}");
 }
 
-void adc(int argc, char **argv) {
+/* -------------------------------------------------------------------------- */
+
+void sh_adc(int argc, char **argv) {
+    uint16_t value = 0;
     switch (argc) {
     case 1: // usage
         print("usage: ");
         println("\tadc read");
-        println("\tadc");
-        println("\tadc");
-        println("\tadc");
-        println("\tadc");
+        println("\tadc settings");
+        println("\tadc set <setting> <value>");
         return;
-    case 2: // adc read
-        if (!strcmp(argv[1], "read")) {
+    case 2:
+        if (!strcmp(argv[1], "read")) { // adc read
             sh_adc_read();
             return;
+
+        } else if (!strcmp(argv[1], "settings")) { // adc settings
+            sh_print_settings();
+            return;
+        }
+        break;
+    case 4: // adc set <setting> <value>
+        if (!strcmp(argv[1], "set")) {
+            if (!strcmp(argv[2], "ADCLK")) { // adc set ADCLK <value>
+                sh_adc_set_clock(atoi(argv[3]));
+                return;
+            } else if (!strcmp(argv[2], "ADPRE")) { // adc set ADPRE <value>
+                sh_adc_set_precharge_time(atoi(argv[3]));
+                return;
+            } else if (!strcmp(argv[2], "ADACQ")) { // adc set ADACQ <value>
+                sh_adc_set_acquisition_time(atoi(argv[3]));
+                return;
+            } else if (!strcmp(argv[2], "ADCAP")) { // adc set ADCAP <value>
+                sh_adc_set_additional_capacitance(atoi(argv[3]));
+                return;
+            }
         }
         break;
     }
